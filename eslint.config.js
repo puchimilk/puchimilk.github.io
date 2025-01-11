9583cc82e06c5bbc02113ac0f9cf85771a5a936e
puchimilk <puchimilk1539@gmail.com>
2025-01-11 18:32:24 +0900

    Prettier を入れる

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
+  eslintConfigPrettier,
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
@@ -25,4 +27,4 @@ export default tseslint.config(
       ],
     },
   },
-)
+);
