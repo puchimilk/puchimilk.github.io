b4c4855ff9c48653a7509a31a74f9f17f44cd047
puchimilk <puchimilk1539@gmail.com>
2025-01-18 03:35:36 +0900

    ESLint の設定を追加

--- eslint.config.js
+++ eslint.config.js
@@ -4,26 +4,42 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginImport from 'eslint-plugin-import';
 
 export default tseslint.config(
   eslintConfigPrettier,
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      eslintPluginImport.flatConfigs.recommended,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      sourceType: 'module',
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+    },
+    settings: {
+      'import/extensions': ['.ts', '.tsx'],
+      'import/resolver': {
+        typescript: { project: './' },
+      },
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
+      ],
+      'import/order': [
+        'error',
+        { 'newlines-between': 'always', alphabetize: { order: 'asc' } },
       ],
     },
   },
