453e7acc30bd515de2c4c73a614075c9189a5c0b
puchimilk <puchimilk1539@gmail.com>
2025-01-18 03:35:23 +0900

    alias を使えるように設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite';
+import path from 'path';
+
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteTsconfigPaths()],
+  resolve: {
+    alias: {
+      '@/': path.resolve(__dirname, './src'),
+    },
+  },
 });
